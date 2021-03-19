59fe946ffc2e0278a45a5f4ca7f402f8f2328f44
IsaiahGz <isaiahgala99@gmail.com>
2021-03-19 17:42:08 -0400

    Link to the Monaco page.

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,11 @@ export default function Home({ allPostsData }) {
 					<a href='https://nextjs.org/learn'>our Next.js tutorial</a>.)
 				</p>
 			</section>
+			<section className={utilStyles.headingMd}>
+				<p>
+					Here is a preview of using <Link href={'/monaco'}>Monaco</Link>{' '}
+				</p>
+			</section>
 			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
 				<h2 className={utilStyles.headingLg}>Blog</h2>
 				<ul className={utilStyles.list}>
--- pages/monaco.js
+++ pages/monaco.js
@@ -1,11 +1,20 @@
 import Editor from '@monaco-editor/react'
+import Link from 'next/link'
 
 export default function MonacoScreen() {
 	return (
-		<Editor
-			height='90vh'
-			defaultLanguage='javascript'
-			defaultValue='// some comment'
-		/>
+		<>
+			<Editor
+				height='90vh'
+				defaultLanguage='javascript'
+				defaultValue='// Welcome to Monaco using JavaScript'
+				theme='vs-dark'
+			/>
+			<div style={{ margin: '1rem 1rem 0' }}>
+				<Link href='/'>
+					<a>← Back to home</a>
+				</Link>
+			</div>
+		</>
 	)
 }
